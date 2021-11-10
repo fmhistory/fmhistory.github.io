38e7b403286e447c72feadc3f02e1db1c82ae5ac
José Miguel Horcas <jhorcas@us.es>
2021-11-10 19:40:41 +0100

    Two examples of timelines with pydot

---
+++ timeline.py
@@ -0,0 +1,14 @@
+import pydot
+
+def draw_graph(filename):
+    graphs = pydot.graph_from_dot_file(filename + ".dot")
+    graph = graphs[0]
+    graph.write_png(filename + ".png")
+
+def main():
+    draw_graph("timeline")
+    draw_graph("timeline2")
+
+
+if __name__ == "__main__":
+    main()
